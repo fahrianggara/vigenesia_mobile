c8fc84b71be77b9f8c44d794532b038794b1c98d
Fahri Anggara <fahriangga30@gmail.com>
2024-11-11 01:24:56 +0700

    remove label nav bottom

--- lib/components/navigation.dart
+++ lib/components/navigation.dart
@@ -5,7 +5,7 @@ import 'package:auto_route/auto_route.dart';
 
 Widget bottomNavigationBar(TabsRouter tabsRouter) {
   return Container(
-    height: 80.0, // Tentukan tinggi bar
+    height: 70.0, // Tentukan tinggi bar
     decoration: BoxDecoration(
       color: HexColor('#FFFFFF'),
       borderRadius: BorderRadius.only(
@@ -21,21 +21,18 @@ Widget bottomNavigationBar(TabsRouter tabsRouter) {
           icon: Icons.home,
           isActive: tabsRouter.activeIndex == 0,
           onTap: () => tabsRouter.setActiveIndex(0),
-          label: 'Beranda', // Menambahkan label
         ),
         // Search Icon
         bottomNavIcon(
           icon: Icons.search_sharp,
           isActive: tabsRouter.activeIndex == 1,
           onTap: () => tabsRouter.setActiveIndex(1),
-          label: 'Pencarian', // Menambahkan label
         ),
         // Profile Icon
         bottomNavIcon(
           icon: Icons.person,
           isActive: tabsRouter.activeIndex == 2,
           onTap: () => tabsRouter.setActiveIndex(2),
-          label: 'Profile', // Menambahkan label
         ),
       ],
     ),
@@ -46,7 +43,6 @@ Widget bottomNavIcon({
   required IconData icon,
   required bool isActive,
   required VoidCallback onTap,
-  required String label, // Tambahkan parameter untuk label
 }) {
   return GestureDetector(
     onTap: onTap,
@@ -54,25 +50,17 @@ Widget bottomNavIcon({
       mainAxisAlignment: MainAxisAlignment.center,
       children: [
         Container(
-          padding: EdgeInsets.symmetric(vertical: 5, horizontal: 20),
+          padding: EdgeInsets.symmetric(vertical: 10, horizontal: 20),
           decoration: BoxDecoration(
             color: isActive ? VColors.primary.withOpacity(0.2) : Colors.transparent,
-            borderRadius: BorderRadius.circular(20), // Border radius persegi panjang
+            borderRadius: BorderRadius.circular(10), // Border radius persegi panjang
           ),
           child: Icon(
             icon,
             color: isActive ? VColors.primary : VColors.border500,
             size: 24, // Ukuran ikon
           ),
-        ),
-        SizedBox(height: 3), // Menambahkan jarak antara ikon dan teks
-        Text(
-          label, // Menampilkan label di bawah ikon
-          style: TextStyle(
-            color: isActive ? VColors.primary : VColors.border500,
-            fontSize: 12, // Ukuran font label
-          ),
-        ),
+        ),// Menambahkan jarak antara ikon dan teks jika aktif
       ],
     ),
   );
